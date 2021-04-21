4a0ed4dc1b21ef73ceb582868f42a49c132f0b97
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-21 12:58:17 -0300

    Saída de dados com as funções print, println e printf do pacote System.out; função para configurar a localização padrão dos valores impressos e concatenação de variáveis em um mesmo comando de escrita.

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int valueOne = 32;
+		double valueTwo = 10.35784;
+		
+		String name = "Lemmy";
+		int age = 70;
+		double earnings = 4000.0;
+		
 		System.out.println("Hello World!");
+		System.out.println("Integer: " + valueOne);
+		System.out.println("Double: " + valueTwo);
+		System.out.printf("Double (Precision 2): %.2f%n", valueTwo);
+		System.out.printf("Double (Precision 4): %.4f\n", valueTwo);
+		
+		Locale.setDefault(Locale.US);
+		System.out.printf("Double with US Locale: %.4f%n", valueTwo);
+		System.out.printf("%s is %d and earns $%.2f.", name, age, earnings);
 	}
 }
