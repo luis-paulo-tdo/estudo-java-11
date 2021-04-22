c360ddeb65230441754b7f116048e7c5efd7a456
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-22 08:32:05 -0300

    Aplicação de switch-case para cenários onde há várias opções de fluxo com base no valor de uma variável (day). Estrutura alternativa aos if-else encadeados.

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -8,14 +8,39 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("How many minutes of phone did you use?");
-		int minutes = scanner.nextInt();
-		double bill = 50.0;
+		System.out.println("What day is today?");
+		System.out.print("> ");
+		int dayNumber = scanner.nextInt();
+		String dayWeek;
 		
-		if (minutes > 100) {
-			bill += (minutes - 100) * 2.0;
+		switch (dayNumber) {
+		case 1:
+			dayWeek = "Sunday";
+			break;
+		case 2:
+			dayWeek = "Monday";
+			break;
+		case 3:
+			dayWeek = "Tuesday";
+			break;
+		case 4:
+			dayWeek = "Wednesday";
+			break;
+		case 5:
+			dayWeek = "Thursday";
+			break;
+		case 6:
+			dayWeek = "Friday";
+			break;
+		case 7:
+			dayWeek = "Saturday";
+			break;
+		default:
+			dayWeek = "Invalid";
+			break;
 		}
-		System.out.printf("Telephone bill value: $%.2f", bill);
+		
+		System.out.println("Day of Week: " + dayWeek);
 		scanner.close();
 	}
 }
