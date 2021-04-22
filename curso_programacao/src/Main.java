13a049afb7fc446ee184926afd99703e35681662
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-22 08:11:30 -0300

    Aplicação de operadores de atribuição cumulativos, onde uma variável recebe o valor dela mesma junto com uma operação aritmética de soma, substração, multiplicação, divisão ou módulo.

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,22 +1,21 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
-		int hour;
 		
-		System.out.println("What time is it?");
-		hour = scanner.nextInt();
+		System.out.println("How many minutes of phone did you use?");
+		int minutes = scanner.nextInt();
+		double bill = 50.0;
 		
-		if (hour < 12) {			
-			System.out.println("Good Morning!");
-		} else if (hour < 18) {				
-			System.out.println("Good Afternoon!");
-		} else {
-			System.out.println("Good Night!");
+		if (minutes > 100) {
+			bill += (minutes - 100) * 2.0;
 		}
+		System.out.printf("Telephone bill value: $%.2f", bill);
 		scanner.close();
 	}
 }
