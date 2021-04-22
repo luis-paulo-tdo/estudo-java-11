f4059f8a1a5bd2ad0186db57b12cd1b0cc4a7b56
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-22 08:01:33 -0300

    Aplicação de estrutura de controle condicional. No exemplo, é uma estrutura condicional composta e encadeada.

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,43 +1,22 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		double valueOne = 3.0;
-		double valueTwo = 4.0;
-		double valueThree = -5.0;
-		double resultOne, resultTwo, resultThree;
+		Scanner scanner = new Scanner(System.in);
+		int hour;
 		
-		System.out.println("VALUES");
-		System.out.println("One:\t" + valueOne);
-		System.out.println("Two:\t" + valueTwo);
-		System.out.println("Three:\t" + valueThree);
-		System.out.println();
+		System.out.println("What time is it?");
+		hour = scanner.nextInt();
 		
-		resultOne = Math.sqrt(valueOne);
-		resultTwo = Math.sqrt(valueTwo);
-		resultThree = Math.sqrt(25.0);
-		System.out.println("SQUARE ROOTS");
-		System.out.println("One:\t" + resultOne);
-		System.out.println("Two:\t" + resultTwo);
-		System.out.println("25.0:\t" + resultThree);
-		System.out.println();
-		
-		resultOne = Math.pow(valueOne, valueTwo);
-		resultTwo = Math.pow(valueOne, 2.0);
-		resultThree = Math.pow(5.0, 2.0);
-		System.out.println("SQUARE ROOTS");
-		System.out.println("One of Two:\t" + resultOne);
-		System.out.println("Two of 2.0:\t" + resultTwo);
-		System.out.println("5.0 of 2.0:\t" + resultThree);
-		System.out.println();
-		
-		resultTwo = Math.abs(valueTwo);
-		resultThree = Math.abs(valueThree);
-		System.out.println("ABSOLUTES");
-		System.out.println("Two:\t" + resultTwo);
-		System.out.println("Three:\t" + resultThree);
+		if (hour < 12) {			
+			System.out.println("Good Morning!");
+		} else if (hour < 18) {				
+			System.out.println("Good Afternoon!");
+		} else {
+			System.out.println("Good Night!");
+		}
+		scanner.close();
 	}
 }
