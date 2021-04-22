c2aca1c9bfffe3b62b5fa33e4e62c0c11476340f
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-22 11:40:59 -0300

    Aplicação de estrutura de repetição while para apresentar o somatório de valores digitados pelo usuário.

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -7,16 +7,23 @@ public class Main {
 		
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
+		int input, sum;
 		
-		System.out.println("Enter Purchase Price");
+		System.out.println("Sum Calc Application");
+		System.out.println("========");
+		System.out.println("1) Enter a sequence of numbers to calculate the sum");
+		System.out.println("2) Enter 0 when you finish");
 		System.out.print("> ");
-		double priceInput = scanner.nextDouble();
-		double price = priceInput > 20.0 ? priceInput * 0.05 : priceInput * 0.1	;
+		input = scanner.nextInt();
+		sum = input;
 		
-		if (priceInput > 20.0) {
-			System.out.println("Discount applied!");
+		while (input != 0) {
+			System.out.print("> ");
+			input = scanner.nextInt();
+			sum += input; 
 		}
-		System.out.printf("Price: $%.2f", price);
+		
+		System.out.println("Sum: " + sum);
 		scanner.close();
 	}
 }
