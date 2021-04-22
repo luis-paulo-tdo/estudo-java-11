8104a2024b6248e5f60b938a4fc059d7f316e804
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-22 19:20:15 -0300

    Aplicação de funções para formatação, recorte, substituição, busca e fragmentação de Strings.

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,27 +1,41 @@
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		Scanner scanner = new Scanner(System.in);
-		int mask = 0b00100000;
-		int number;
+		String textOriginal = "abcde FGHIJ ABC abc DEFG     ";
+		String textLower = textOriginal.toLowerCase();
+		String textUpper = textOriginal.toUpperCase();
+		String textTrimmed = textOriginal.trim();
 		
-		System.out.println("The 6th bit boolean");
-		System.out.println("===================");
-		System.out.println("Type a number:");
-		System.out.print("> ");
-		number = scanner.nextInt();
+		String textSubstringOne = textOriginal.substring(2);
+		String textSubstringTwo = textOriginal.substring(2, 9);
 		
-		if ((number & mask) != 0) {
-			System.out.println("The 6th bit is true!");
-		} else {
-			System.out.println("The 6th bit is false!");
-		}
+		String textReplacedOne = textOriginal.replace('a', 'x');
+		String textReplacedTwo = textOriginal.replace("abc", "xy");
 		
-		scanner.close();
+		int textIndex = textOriginal.indexOf("bc");
+		int textLastIndex = textOriginal.lastIndexOf("bc");
+		String[] textVector = textOriginal.split("bc");
+		
+		System.out.println("String Functions");
+		System.out.println("==================================================");
+		System.out.println("Original Value:\t\t[" + textOriginal + "]\n");
+		
+		System.out.println("Format toLowerCase:\t[" + textLower + "]");
+		System.out.println("Format toUpperCase:\t[" + textUpper + "]");
+		System.out.println("Format trim:\t\t[" + textTrimmed + "]\n");
+		
+		System.out.println("Cut substring(2):\t[" + textSubstringOne + "]");
+		System.out.println("Cut substring(2, 9):\t[" + textSubstringTwo + "]\n");
+		
+		System.out.println("Replace a to x:\t\t[" + textReplacedOne + "]");
+		System.out.println("Replace abc to xy:\t[" + textReplacedTwo + "]\n");
+		
+		System.out.println("Index of bc:\t\t[" + textIndex + "]");
+		System.out.println("Last Index of bc:\t[" + textLastIndex + "]\n");
+		
+		System.out.println("Vector Index 0:\t\t[" + textVector[0] + "]");
+		System.out.println("Vector Index 1:\t\t[" + textVector[1] + "]");
+		System.out.println("Vector Index 2:\t\t[" + textVector[2] + "]");
 	}
 }
