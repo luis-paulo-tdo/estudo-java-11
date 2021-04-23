0ba4f7937662b824562a0ddb6b7a51e416dca045
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-23 08:40:30 -0300

    Criação de projeto para gerenciar produtos em estoque utilizando a Classe produtos para obter os dados de entrada de usuário. A classe tem os métodos de adicionar e remover produtos em estoque, assim como calcular o total monetário de produtos estocados.

---
+++ projeto_08_02_poo_estoque/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		Product product = new Product();
+		
+		System.out.println("Stock Product Management");
+		System.out.println("========================");
+		System.out.println("Enter product data");
+		
+		System.out.print("Name:");
+		product.name = scanner.next();
+		
+		System.out.print("Price:");
+		product.price = scanner.nextDouble();
+		
+		System.out.print("Quantity In Stock:");
+		product.quantity = scanner.nextInt();
+		
+		scanner.close();
+	}
+}
