f23ca064e95d3884ed3d4434cd98f0ca670ba583
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-23 12:40:08 -0300

    Novo projeto para aplicar método Construtor, obrigando que os produtos instanciados sejam inicializados com os valores de seus atributos. Não faz sentido ter objetos de Produtos instanciados com os valores padrão em seus Atributos.

---
+++ projeto_09_01_construtores/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		Product product;
+		String name;
+		double price;
+		int quantity;
+		
+		System.out.println("Stock Product Management");
+		System.out.println("=====================================================");
+		System.out.println("Enter product data");
+		
+		System.out.print("Name: ");
+		name = scanner.next();
+		
+		System.out.print("Price: ");
+		price = scanner.nextDouble();
+		
+		System.out.print("Quantity In Stock: ");
+		quantity = scanner.nextInt();
+		
+		product = new Product(name, price, quantity);
+		System.out.println("Product Data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be added in stock: ");
+		product.addProducts(scanner.nextInt());
+		
+		System.out.println("Updated Data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be removed from stock: ");
+		product.removeProducts(scanner.nextInt());
+		
+		System.out.println("Updated Data: " + product);
+		
+		scanner.close();
+	}
+}
