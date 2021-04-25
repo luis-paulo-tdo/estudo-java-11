e40c5d187a3ee2ac0bf9b10ba58ffcdad9c7449c
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-25 09:17:46 -0300

    Trabalhando com datas no Java. Uma data na programação representa um Instante, e a classe Date é a forma mais clássica de se trabalhar com datas no Java. O valor que é armazenado é a quantidade de milissegundos desde a meia noite do dia 1 de janeiro de 1970. O fuso horário padrão é o GMT (horário de Greenwich) e o formato padrão é o UTC (Univernal Time Coordinate). Já a classe SimpleDateFormat ajuda a definir formatos para conversão de Date e String. Datas em formato String são ideais para a entrada e a saída de dados, enquanto o processamento torna-se mais fácil ao tratá-las como um objeto. As datas em texto geralmente são armazenadas no padrão ISO 8601 (yyyy-MM-ddTHH:mm:ssZ) onde o caractere final 'Z' indica que o formato utilizado é o UTC. A classe Instant, por sua vez, pertence a uma biblioteca própria do Java criada para se trabalhar melhor com datas, e com ela, torna-se possível converter formatos locais (BRT, etc.) em formato UTC. O construtor padrão da classe Date já armazena o instante atual. No exemplo do código, foram impressas datas no formato padrão da máquina local (BRT), as datas formatas com o SimpleDateFormat e as datas convertidas em UTC.

---
+++ projeto_11_01_date/src/application/Program.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat simpleDateFormatOne = new SimpleDateFormat("dd/MM/yyy");
+		SimpleDateFormat simpleDateFormatTwo = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+		SimpleDateFormat simpleDateFormatThree = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+		simpleDateFormatThree.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		Date dateOne = simpleDateFormatOne.parse("25/04/2021");
+		Date dateTwo = simpleDateFormatTwo.parse("25/04/2021 08:54:45");
+		
+		Date currentDateOne = new Date();
+		Date currentDateTwo = new Date(System.currentTimeMillis());
+		
+		Date instantZero = new Date(0L);
+		Date hourFive = new Date(1000L * 60 * 60 * 5);
+		
+		Date iso8601Date = Date.from(Instant.parse("2021-04-25T09:05:25Z"));
+		
+		System.out.println("Dates on BRT");
+		System.out.println("===================================");
+		System.out.println("Date One:\t\t" + simpleDateFormatTwo.format(dateOne));
+		System.out.println("Date Two:\t\t" + simpleDateFormatTwo.format(dateTwo));
+		System.out.println("Current Date One:\t" + simpleDateFormatTwo.format(currentDateOne));
+		System.out.println("Current Date Two:\t" + simpleDateFormatTwo.format(currentDateTwo));
+		System.out.println("Instant Zero:\t\t" + simpleDateFormatTwo.format(instantZero));
+		System.out.println("Hour Five:\t\t" + simpleDateFormatTwo.format(hourFive));
+		System.out.println("Hour Five:\t\t" + simpleDateFormatTwo.format(iso8601Date));
+		
+		System.out.println();
+		System.out.println("Dates on UTC");
+		System.out.println("===================================");
+		System.out.println("Date One:\t\t" + simpleDateFormatThree.format(dateOne));
+		System.out.println("Date Two:\t\t" + simpleDateFormatThree.format(dateTwo));
+		System.out.println("Current Date One:\t" + simpleDateFormatThree.format(currentDateOne));
+		System.out.println("Current Date Two:\t" + simpleDateFormatThree.format(currentDateTwo));
+		System.out.println("Instant Zero:\t\t" + simpleDateFormatThree.format(instantZero));
+		System.out.println("Hour Five:\t\t" + simpleDateFormatThree.format(hourFive));
+		System.out.println("Hour Five:\t\t" + simpleDateFormatThree.format(iso8601Date));
+		
+		System.out.println();
+		System.out.println("Raw Dates");
+		System.out.println("===================================");
+		System.out.println("Date One:\t\t" + dateOne);
+		System.out.println("Date Two:\t\t" + dateTwo);
+		System.out.println("Current Date One:\t" + currentDateOne);
+		System.out.println("Current Date Two:\t" + currentDateTwo);
+		System.out.println("Instant Zero:\t\t" + instantZero);
+		System.out.println("Hour Five:\t\t" + hourFive);
+		System.out.println("Hour Five:\t\t" + iso8601Date);
+	}
+
+}
