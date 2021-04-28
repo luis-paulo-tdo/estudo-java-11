9eeea88e313ab3ff3e29721746cab16b0f958fa5
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-04-28 12:46:14 -0300

    Utilização do recurso de impressão da pilha de chamadas de métodos da aplicação (stack trace) no momento em que uma Exceção é lançada, permitindo que o desenvolvedor visualize todas as chamadas de Método que culminaram no lançamento.

--- projeto_15_03_excecao/src/application/Program.java
+++ projeto_15_03_excecao/src/application/Program.java
@@ -7,6 +7,20 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Start of Program.");
+		callNavigate();
+		System.out.println("End of Program.");
+	}
+	
+	public static void callNavigate() {
+		System.out.println("====> Call Navigate Start <====");
+		navigateOnText();
+		System.out.println("====> Call Navigate End <====");
+	}
+	
+	public static void navigateOnText() {
+		
+		System.out.println("====> Navigate On Text Start <====");
 		Scanner scanner = new Scanner(System.in);
 		String[] vector;
 		int position;
@@ -21,12 +35,15 @@ public class Program {
 			System.out.println("Vector on [" + position + "]: " + vector[position]);
 		} catch (ArrayIndexOutOfBoundsException exception) {
 			System.out.println("Error: Invalid position!");
+			exception.printStackTrace();
+			scanner.next();
 		} catch (InputMismatchException exception) {
 			System.out.println("Error: Invalid input!");
+			exception.printStackTrace();
+			scanner.next();
 		}
 		
-		System.out.println("End of Program.");
 		scanner.close();
+		System.out.println("====> Navigate On Text End <====");
 	}
-
 }
