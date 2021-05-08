9303b65746e0f7f027f6759c518586be1386266d
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-05-08 14:09:37 -0300

    Iniciando a implementação das Classes Board (Tabuleiro) e Piece (Peças).

--- projeto_16_01_chess_match_system/src/application/Program.java
+++ projeto_16_01_chess_match_system/src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Board board = new Board(8, 8);
 		Position position = new Position(3, 5);
+		
 		System.out.println("Welcome To The Chess Match!");
+		System.out.println();
+		System.out.println("Board Size: [" + board.getRows() + ", " + board.getColumns() + "]");
 		System.out.println("Current Position: " + position);
 	}
 
