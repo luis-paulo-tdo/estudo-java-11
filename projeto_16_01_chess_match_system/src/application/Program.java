2448b3e4c5dcc1f759489a9281f20e5663906d36
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-05-11 00:01:16 -0300

    Método para limpeza de tela e tratamento de exceções de forma mais amigável.

--- projeto_16_01_chess_match_system/src/application/Program.java
+++ projeto_16_01_chess_match_system/src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -17,16 +19,26 @@ public class Program {
 		System.out.println();
 		
 		while (true) {
-			UserInterface.printBoard(chessMatch.getPieces());
 			
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UserInterface.readChessPosition(scanner);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UserInterface.readChessPosition(scanner);
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				UserInterface.clearScreen();
+				UserInterface.printBoard(chessMatch.getPieces());
+				
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UserInterface.readChessPosition(scanner);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UserInterface.readChessPosition(scanner);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException exception) {
+				System.out.println(exception.getMessage());
+				scanner.nextLine();
+			} catch (InputMismatchException exception) {
+				System.out.println(exception.getMessage());
+				scanner.nextLine();
+			}
 		}
 	}
 
