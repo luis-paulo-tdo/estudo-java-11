4b92bd1b313dc9c41924663f34c1c01703bddc72
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-05-19 08:08:02 -0300

    Implementação de Método para verificar se o Rei pode realizar movimentos (posição deve estar vazia ou com Peça oponente) e implementação de lógica para os movimentos do Rei (apenas uma casa para qualquer uma das 8 direções).

--- projeto_16_01_chess_match_system/src/chess/pieces/King.java
+++ projeto_16_01_chess_match_system/src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -11,13 +12,68 @@ public class King extends ChessPiece {
 	}
 
 	@Override
+	public boolean[][] possibleMoves() {
+		Position testPosition = new Position(0, 0);
+		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		// Above
+		testPosition.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// Left
+		testPosition.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// Right
+		testPosition.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// Below
+		testPosition.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// North West
+		testPosition.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// North East
+		testPosition.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// South West
+		testPosition.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		// South East
+		testPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(testPosition) && canMove(testPosition)) {
+			possibleMoves[testPosition.getRow()][testPosition.getColumn()] = true;
+		}
+		
+		return possibleMoves;
+	}
+	
+	@Override
 	public String toString() {
 		return "K";
 	}
-
-	@Override
-	public boolean[][] possibleMoves() {
-		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		return possibleMoves;
+	
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece == null || piece.getColor() != getColor();
 	}
 }
