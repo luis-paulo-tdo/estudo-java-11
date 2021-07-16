fd3cf1e0deb77c637919f2c8119110d45a4a197c
Luis Paulo <luis.paulo.tdo@gmail.com>
2021-07-16 06:55:49 -0300

    Criação da Classe Queen para representar a Peça da Rainha e calcular seus possíveis movimentos. As Peças da Rainha também foram posicionadas no Tabuleiro. Uma Rainha pode se mover tanto pelas laterias, tal como uma Torre, quanto pelas diagonais, tal como um Bispo.

--- projeto_16_01_chess_match_system/src/chess/pieces/Queen.java
+++ projeto_16_01_chess_match_system/src/chess/pieces/Queen.java
@@ -5,9 +5,9 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
-	
-	public Rook(Board board, Color color) {
+public class Queen extends ChessPiece {
+
+	public Queen(Board board, Color color) {
 		super(board, color);
 	}
 	
@@ -102,6 +102,6 @@ public class Rook extends ChessPiece {
 	
 	@Override
 	public String toString() {
-		return "R";
+		return "Q";
 	}
 }
